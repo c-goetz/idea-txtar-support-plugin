480bb718cdba7e025f1aeedd68cab99196293f72
Carlo Goetz <carlo.goetz@inovex.de>
2026-07-14 22:01:26 +0200

    add suggested manipulator, also use filename to determine filetype

--- src/main/kotlin/com/arran4/txtar/FileElement.kt
+++ src/main/kotlin/com/arran4/txtar/FileElement.kt
@@ -9,7 +9,7 @@ import com.intellij.psi.LiteralTextEscaper
 import com.intellij.psi.PsiLanguageInjectionHost
 import com.intellij.psi.impl.source.tree.injected.InjectionBackgroundSuppressor
 
-class FileElement(node: ASTNode) : ASTWrapperPsiElement(node),  PsiLanguageInjectionHost, InjectionBackgroundSuppressor {
+class FileElement(node: ASTNode) : ASTWrapperPsiElement(node), PsiLanguageInjectionHost, InjectionBackgroundSuppressor {
     override fun isValidHost(): Boolean {
         return true
     }
@@ -21,14 +21,24 @@ class FileElement(node: ASTNode) : ASTWrapperPsiElement(node),  PsiLanguageInjec
     override fun createLiteralTextEscaper(): LiteralTextEscaper<out PsiLanguageInjectionHost?> {
         return LiteralTextEscaper.createSimple(this)
     }
-    internal class Manipulator: AbstractElementManipulator<FileElement>() {
+
+    internal class Manipulator : AbstractElementManipulator<FileElement>() {
         override fun handleContentChange(
             element: FileElement,
             range: TextRange,
             newContent: String?
         ): FileElement? {
-            // no manipulation necessary
-            return element
+            val oldText = element.text
+            val newText =
+                oldText.substring(0, range.startOffset) + (newContent ?: "") + oldText.substring(range.endOffset)
+            val dummyFile = com.intellij.psi.PsiFileFactory.getInstance(element.project)
+                .createFileFromText("dummy.txtar", TxtarLanguage.INSTANCE, "-- dummy --\n$newText")
+            val newElement = com.intellij.psi.util.PsiTreeUtil.findChildOfType(dummyFile, FileElement::class.java)
+            return if (newElement != null) {
+                element.replace(newElement) as FileElement
+            } else {
+                element
+            }
         }
     }
 }
--- src/main/kotlin/com/arran4/txtar/FileInjector.kt
+++ src/main/kotlin/com/arran4/txtar/FileInjector.kt
@@ -1,7 +1,5 @@
 package com.arran4.txtar
 
-import com.intellij.lang.Language
-import com.intellij.lang.LanguageParserDefinitions
 import com.intellij.lang.LanguageUtil
 import com.intellij.lang.injection.general.Injection
 import com.intellij.lang.injection.general.LanguageInjectionContributor
@@ -14,15 +12,13 @@ class FileInjector : LanguageInjectionContributor {
     override fun getInjection(context: PsiElement): Injection? {
         val type = context.elementType ?: return null
         if (type != TxtarElementTypes.FILE_CONTENT) return null
-        val header = context.prevSibling.takeIf { it.elementType == TxtarElementTypes.HEADER } ?: return null
+        val header = context.parent.firstChild.takeIf { it.elementType == TxtarElementTypes.HEADER } ?: return null
         val text = header.text ?: return null
         val stripped = text.trim().removePrefix("-- ").removeSuffix(" --").trim()
-        val idx = stripped.lastIndexOf('.')
-        if (idx == -1) {
-            return null
-        }
-        val extension = stripped.substring(idx + 1)
-        val fileType = FileTypeManager.getInstance().getFileTypeByExtension(extension)
+        val fileName = stripped.substringAfterLast('/')
+        val extension = stripped.substringAfterLast('.')
+        val ftManager = FileTypeManager.getInstance()
+        val fileType = ftManager.findFileTypeByName(fileName) ?: ftManager.getFileTypeByExtension(extension)
         val language =  LanguageUtil.getFileTypeLanguage(fileType) ?: return null
         return SimpleInjection(language, "", "", null)
     }
